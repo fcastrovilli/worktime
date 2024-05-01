d7e983fef4d11f93f151014ca31da1c446c9e557
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-05-01 19:51:46 +0200

    feat: crud panel component

---
+++ src/lib/components/crud/panel.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import * as Sheet from '$lib/components/ui/sheet/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+	export let open_label = 'Open';
+	export let title = 'Create';
+	export let description = "Make changes here. Click save when you're done.";
+	export let triggerClass = '';
+	export let open = false;
+</script>
+
+<Sheet.Root bind:open>
+	<Sheet.Trigger asChild let:builder>
+		<Button builders={[builder]} variant="outline" class={triggerClass}>
+			<slot name="trigger">{open_label}</slot>
+		</Button>
+	</Sheet.Trigger>
+	<Sheet.Content side="right">
+		<Sheet.Header>
+			<Sheet.Title>{title}</Sheet.Title>
+			<Sheet.Description>
+				{description}
+			</Sheet.Description>
+		</Sheet.Header>
+		<div class="grid gap-4 py-4">
+			<slot name="form" />
+		</div>
+	</Sheet.Content>
+</Sheet.Root>
