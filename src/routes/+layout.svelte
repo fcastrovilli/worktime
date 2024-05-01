566e14cc26f3e513c185a55f40996ac0ffc67fe8
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-05-01 19:53:30 +0200

    feat: toast notifications

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import Nav from '$lib/components/nav/nav.svelte';
+	import { Toaster } from '$lib/components/ui/sonner';
 	import '../app.pcss';
 	export let data;
 </script>
 
 <Nav user={data.user} />
+<Toaster />
 <slot></slot>
