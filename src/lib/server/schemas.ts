2794cd00810d80df6147d433f4e5917f14e6336e
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-05-01 19:53:08 +0200

    feat: added clients, worksessions and projects tables with correct relations

--- src/lib/server/schemas.ts
+++ src/lib/server/schemas.ts
@@ -1,4 +1,4 @@
-import type { InferSelectModel } from 'drizzle-orm';
+import { relations, type InferSelectModel } from 'drizzle-orm';
 
 import { pgTable, text, timestamp, integer } from 'drizzle-orm/pg-core';
 
@@ -22,6 +22,12 @@ export const usersTable = pgTable('user', {
 	...timestamps
 });
 
+export const usersRelations = relations(usersTable, ({ many }) => ({
+	clients: many(clientsTable),
+	worksessions: many(worksessionsTable),
+	projects: many(projectsTable)
+}));
+
 /**
  * Sessions are used to store the user's session information.
  */
@@ -35,5 +41,80 @@ export const sessionsTable = pgTable('session', {
 		mode: 'date'
 	}).notNull()
 });
+
+export const clientsTable = pgTable('client', {
+	id: text('id').notNull().primaryKey(),
+	name: text('name').notNull().unique(),
+	email: text('email').unique(),
+	details: text('details'),
+	website: text('website'),
+	user_id: text('user_id')
+		.notNull()
+		.references(() => usersTable.id),
+	...timestamps
+});
+
+export const clientsRelations = relations(clientsTable, ({ one, many }) => ({
+	user: one(usersTable, { fields: [clientsTable.user_id], references: [usersTable.id] }),
+	projects: many(projectsTable)
+}));
+
+export const worksessionsTable = pgTable('worksession', {
+	id: text('id').notNull().primaryKey(),
+	detail: text('detail'),
+	start: timestamp('start', {
+		withTimezone: true,
+		mode: 'date'
+	}).notNull(),
+	end: timestamp('end', {
+		withTimezone: true,
+		mode: 'date'
+	}).notNull(),
+	project_id: text('project_id')
+		.notNull()
+		.references(() => projectsTable.id),
+	client_id: text('client_id')
+		.notNull()
+		.references(() => clientsTable.id),
+	user_id: text('user_id')
+		.notNull()
+		.references(() => usersTable.id),
+	...timestamps
+});
+
+export const worksessionsRelations = relations(worksessionsTable, ({ one }) => ({
+	clients: one(clientsTable, {
+		fields: [worksessionsTable.client_id],
+		references: [clientsTable.id]
+	}),
+	projects: one(projectsTable, {
+		fields: [worksessionsTable.project_id],
+		references: [projectsTable.id]
+	}),
+	users: one(usersTable, { fields: [worksessionsTable.user_id], references: [usersTable.id] })
+}));
+
+export const projectsTable = pgTable('project', {
+	id: text('id').notNull().primaryKey(),
+	name: text('name').notNull().unique(),
+	description: text('description'),
+	deadline: timestamp('deadline', {
+		withTimezone: true,
+		mode: 'date'
+	}),
+	client_id: text('client_id')
+		.notNull()
+		.references(() => clientsTable.id),
+	user_id: text('user_id')
+		.notNull()
+		.references(() => usersTable.id),
+	...timestamps
+});
+
+export const projectsRelations = relations(projectsTable, ({ many, one }) => ({
+	worksessions: many(worksessionsTable),
+	clients: one(clientsTable, { fields: [projectsTable.client_id], references: [clientsTable.id] }),
+	users: one(usersTable, { fields: [projectsTable.user_id], references: [usersTable.id] })
+}));
 
 export type User = InferSelectModel<typeof usersTable>;
