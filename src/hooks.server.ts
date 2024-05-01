78d56a15c571e0d3d161cf05b6d6299e0c791671
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-05-01 19:51:28 +0200

    feat: !(auth) folder redirect if not logged

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,12 @@
 import { lucia } from '$lib/server/auth';
-import type { Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const sessionId = event.cookies.get(lucia.sessionCookieName);
+	if (!event.route.id?.startsWith('/(auth)') && !sessionId) {
+		redirect(302, '/signup');
+	}
+
 	if (!sessionId) {
 		event.locals.user = null;
 		event.locals.session = null;
@@ -24,6 +28,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 			...sessionCookie.attributes
 		});
 	}
+
 	event.locals.user = user;
 	event.locals.session = session;
 	return resolve(event);
