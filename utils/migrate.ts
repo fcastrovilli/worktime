18c9a8c81725645e00a53c7f831a6478be542a01
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-05-04 00:10:07 +0200

    feat: drizzle migrations

---
+++ utils/migrate.ts
@@ -0,0 +1,22 @@
+import { drizzle } from 'drizzle-orm/neon-http';
+import { migrate } from 'drizzle-orm/neon-http/migrator';
+import { neon } from '@neondatabase/serverless';
+
+const sql = neon(process.env.DATABASE_URL as string);
+
+const db = drizzle(sql);
+
+const main = async () => {
+	try {
+		await migrate(db, {
+			migrationsFolder: './drizzle/'
+		});
+
+		console.log('Migration successful 🎉');
+	} catch (error) {
+		console.error(error);
+		process.exit(1);
+	}
+};
+
+main();
