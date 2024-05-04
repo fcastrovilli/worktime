a78e3c6102cd3f7ae4b28f5bcdd4dc74dafd36e2
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-05-04 11:04:04 +0200

    fix: default user & improved seed script

--- utils/seed.ts
+++ utils/seed.ts
@@ -1,15 +1,29 @@
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import * as schema from '../src/lib/server/schemas';
-import { generateIdFromEntropySize } from 'lucia';
+import { generateId, generateIdFromEntropySize } from 'lucia';
 import { faker } from '@faker-js/faker';
 import slugify from 'slugify';
 
 const sql = neon(process.env.DATABASE_URL as string);
 
+type DefaultUser = {
+	username: string;
+	github_id: number;
+	fullname: string;
+	avatar: string;
+};
+
+const defaultUser: DefaultUser = JSON.parse(process.env.DEFAULT_USER as string);
+
 const db = drizzle(sql, {
 	schema
 });
+
+const users: schema.User[] = [];
+const clients: schema.Client[] = [];
+const projects: schema.Project[] = [];
+const worksessions: schema.Worksession[] = [];
 
 const main = async () => {
 	try {
@@ -21,66 +35,35 @@ const main = async () => {
 		await db.delete(schema.clientsTable);
 		await db.delete(schema.usersTable);
 
-		const users: schema.User[] = [];
-		const clients: schema.Client[] = [];
-		const projects: schema.Project[] = [];
-		const worksessions: schema.Worksession[] = [];
+		// Create default user
+		if (defaultUser) {
+			const defaultUserId = generateIdFromEntropySize(10);
+			users.push({
+				id: defaultUserId,
+				username: defaultUser.username,
+				github_id: defaultUser.github_id,
+				fullname: defaultUser.fullname,
+				avatar: defaultUser.avatar,
+				createdAt: faker.date.anytime(),
+				updatedAt: faker.date.anytime()
+			});
+			seedDB(defaultUserId);
+		}
 
-		for (let i = 0; i < 3; i++) {
+		// Create 5 users
+		for (let i = 0; i < 5; i++) {
 			const userId = generateIdFromEntropySize(10);
 			users.push({
 				id: userId,
-				username: faker.person.firstName(),
-				github_id: faker.number.int(15),
+				username: faker.person.firstName().toLowerCase(),
+				github_id: Math.floor(Math.random() * 100000000),
 				fullname: faker.person.fullName(),
 				avatar: faker.image.avatarGitHub(),
 				createdAt: faker.date.anytime(),
 				updatedAt: faker.date.anytime()
 			});
-			for (let j = 0; j < 5; j++) {
-				const clientId = generateIdFromEntropySize(10);
-				const projectId = generateIdFromEntropySize(10);
 
-				const randomWorksessions = Math.floor(Math.random() * 5);
-
-				clients.push({
-					id: clientId,
-					name: faker.company.name(),
-					email: faker.internet.email(),
-					details: faker.company.catchPhrase(),
-					website: faker.internet.url(),
-					slug: slugify(faker.company.name().toLowerCase()),
-					user_id: userId,
-					createdAt: faker.date.anytime(),
-					updatedAt: faker.date.anytime()
-				});
-
-				projects.push({
-					id: projectId,
-					name: faker.company.name(),
-					description: faker.lorem.paragraph(),
-					deadline: faker.date.future(),
-					slug: slugify(faker.company.name().toLowerCase()),
-					client_id: clientId,
-					user_id: userId,
-					createdAt: faker.date.anytime(),
-					updatedAt: faker.date.anytime()
-				});
-
-				for (let j = 0; j < randomWorksessions; j++) {
-					worksessions.push({
-						id: generateIdFromEntropySize(10),
-						detail: faker.lorem.sentence(),
-						start: faker.date.past(),
-						end: faker.date.future(),
-						project_id: projectId,
-						client_id: clientId,
-						user_id: userId,
-						createdAt: faker.date.anytime(),
-						updatedAt: faker.date.anytime()
-					});
-				}
-			}
+			seedDB(userId);
 		}
 
 		await db.insert(schema.usersTable).values(users);
@@ -96,3 +79,53 @@ const main = async () => {
 };
 
 main();
+
+function seedDB(userId: string) {
+	// Create max 5 clients per user
+	for (let j = 0; j < Math.floor(Math.random() * 5 + 1); j++) {
+		const clientId = generateId(15);
+
+		clients.push({
+			id: clientId,
+			name: faker.company.name(),
+			email: faker.internet.email(),
+			details: faker.company.catchPhrase(),
+			website: faker.internet.url(),
+			slug: slugify(faker.company.name().toLowerCase()),
+			user_id: userId,
+			createdAt: faker.date.anytime(),
+			updatedAt: faker.date.anytime()
+		});
+
+		// Create max 5 projects per client
+		for (let j = 0; j < Math.floor(Math.random() * 5 + 1); j++) {
+			const projectId = generateId(15);
+			projects.push({
+				id: projectId,
+				name: faker.company.name(),
+				description: faker.lorem.paragraph(),
+				deadline: faker.date.future(),
+				slug: slugify(faker.company.name().toLowerCase()),
+				client_id: clientId,
+				user_id: userId,
+				createdAt: faker.date.anytime(),
+				updatedAt: faker.date.anytime()
+			});
+
+			// Create max 5 worksessions per project
+			for (let j = 0; j < Math.floor(Math.random() * 5 + 1); j++) {
+				worksessions.push({
+					id: generateId(15),
+					detail: faker.lorem.sentence(),
+					start: faker.date.past(),
+					end: faker.date.future(),
+					project_id: projectId,
+					client_id: clientId,
+					user_id: userId,
+					createdAt: faker.date.anytime(),
+					updatedAt: faker.date.anytime()
+				});
+			}
+		}
+	}
+}
