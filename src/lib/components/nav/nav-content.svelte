e1679cc53dbf223722e4fac2a650e49bbdb0f25a
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-05-07 20:58:43 +0200

    fix: reactive headbar

--- src/lib/components/nav/nav-content.svelte
+++ src/lib/components/nav/nav-content.svelte
@@ -3,26 +3,27 @@
 
 	let className: string | undefined | null = undefined;
 	export { className as class };
+	import { page } from '$app/stores';
+
+	const pages: { href: string; title: string }[] = [
+		{ href: '/', title: 'Dashboard' },
+		{ href: '/sessions', title: 'Sessions' },
+		{ href: '/projects', title: 'Projects' },
+		{ href: '/clients', title: 'Clients' }
+	];
+
+	$: currentPage = $page.url.pathname;
 </script>
 
 <nav class={cn('flex items-center space-x-4 lg:space-x-6', className)}>
-	<a href="/" class="text-sm font-medium transition-colors hover:text-primary"> Dashboard </a>
-	<a
-		href="/sessions"
-		class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-	>
-		Sessions
-	</a>
-	<a
-		href="/projects"
-		class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-	>
-		Projects
-	</a>
-	<a
-		href="/clients"
-		class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-	>
-		Clients
-	</a>
+	{#each pages as page}
+		<a
+			href={page.href}
+			class="text-sm font-medium {currentPage.includes(page.href)
+				? ''
+				: 'text-muted-foreground'} transition-colors hover:text-primary"
+		>
+			{page.title}
+		</a>
+	{/each}
 </nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
+<script lang="ts">
+	import Bar from '$lib/components/crud/bar.svelte';
+</script>
+
 <div class="container flex w-screen items-center justify-center py-10">
 	<h1 class="text-center text-4xl">Welcome to <span class="font-bold">WORKTIME</span></h1>
 </div>
+<Bar />
