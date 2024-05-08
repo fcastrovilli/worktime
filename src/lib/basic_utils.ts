1bd9f70fb9f59e4aa601b99896cea898be89a01f
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-05-08 19:27:00 +0200

    feat: seconds as string output

--- src/lib/basic_utils.ts
+++ src/lib/basic_utils.ts
@@ -4,13 +4,14 @@ export const calculateElapsedTime = (start: Date | undefined, end: Date | undefi
 	const days = Math.floor(elapsedTime / (1000 * 60 * 60 * 24));
 	const hours = Math.floor((elapsedTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 	const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
+	const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
 
-	//build return string .make sure there are no 0 values on days, hours and minutes variables
 	const daysString = days > 0 ? `${days}d` : '';
 	const hoursString = hours > 0 ? `${hours}h` : '';
 	const minutesString = minutes > 0 ? `${minutes}m` : '';
+	const secondsString = seconds > 0 ? `${seconds}s` : '';
 
-	return `${daysString} ${hoursString} ${minutesString}`;
+	return `${daysString} ${hoursString} ${minutesString} ${secondsString}`;
 };
 
 export type BasicType = {
